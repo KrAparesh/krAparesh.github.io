c4d08ab50d9e800df2a807efa68c35bc27b0cc4d
APARESH <kraparesh@gmail.com>
2023-02-18 21:55:39 +0530

    Done with static website

--- main.js
+++ main.js
@@ -8,5 +8,16 @@ $(document).ready(() => {
         } else {
             $("header").removeClass("sticky");
         }
-    })
+    });
+
+    $(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
 })
