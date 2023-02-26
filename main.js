700275e92da7ad5c945c181d26e6f32bf57e9287
APARESH <kraparesh@gmail.com>
2023-02-26 17:27:50 +0530

    Add/Fix: Re-worked on buttons

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ $(document).ready(() => {
 		});
 	});
 });
+
 function myFunction() {
     var x = document.querySelector(".nav-items");
     var y = document.querySelector(".hamburger");
@@ -30,3 +31,35 @@ function myFunction() {
 
 
 document.querySelectorAll(".nav-items li a").forEach((item) => item.onclick = myFunction);
+
+//active section
+
+var navLinks = document.querySelectorAll("nav ul li a");
+var sections = document.querySelectorAll("section");
+
+// Update the active section when the user scrolls
+window.addEventListener("scroll", function () {
+    // Get the current scroll position
+    var scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Loop through the sections and check which one is currently visible
+    for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+        var sectionTop = section.offsetTop;
+        var sectionBottom = sectionTop + section.offsetHeight;
+
+        if (scrollPos >= sectionTop && scrollPos < sectionBottom) {
+            // If the section is visible, add the "active" class to the corresponding link in the navbar
+            var link = document.querySelector('nav ul li a[href="#' + section.id + '"]');
+            if (link) {
+                link.classList.add("active");
+            }
+        } else {
+            // If the section is not visible, remove the "active" class from the corresponding link in the navbar
+            var link = document.querySelector('nav ul li a[href="#' + section.id + '"]');
+            if (link) {
+                link.classList.remove("active");
+            }
+        }
+    }
+});
